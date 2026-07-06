ca71be4bc1e6009e4c15fb8eb4365a031b2b86eb
pab-h <dev.pab.2020@gmail.com>
2026-07-06 16:02:16 -0300

    feat: script para baixar o PlantVillage

--- scripts/download_plant_village_dataset.py
+++ scripts/download_plant_village_dataset.py
@@ -1,1 +1,5 @@
-# Faça um código que baixe o dataset Plant Village
+from datasets import load_dataset
+
+dataset = load_dataset("mohanty/PlantVillage", "default")
+
+dataset.save_to_disk("data/huggingface/PlantVillage")
